c6456cc0cdf03d3b13f70849afacc485866ccbc8
saigakov <58504255+saigakov@users.noreply.github.com>
2020-05-26 18:04:52 +0300

    Add files via upload

---
+++ malysh/js/script.js
@@ -0,0 +1,17 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const list = document.querySelector('.header__list'),
+        menuItem = document.querySelectorAll('.header__item'),
+        hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        list.classList.toggle('header__list_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            list.classList.toggle('header__list_active');
+        })
+    })
+})
